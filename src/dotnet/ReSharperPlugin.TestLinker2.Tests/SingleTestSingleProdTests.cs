ede53b84d1e912350e5de924b48c43c37a0388f4
Yarin Omesi <yarinomesi@gmail.com>
2022-11-08 00:30:28 +0200

    Add Scoped Namespace To All Test Classes To Make Them Compile

--- src/dotnet/ReSharperPlugin.TestLinker2.Tests/SingleTestSingleProdTests.cs
+++ src/dotnet/ReSharperPlugin.TestLinker2.Tests/SingleTestSingleProdTests.cs
@@ -1,17 +1,18 @@
 ﻿using JetBrains.ReSharper.IntentionsTests.Navigation;
 using NUnit.Framework;
 
-namespace ReSharperPlugin.TestLinker2.Tests;
-
-public class SingleTestSingleProdTests : AllNavigationProvidersTestBase
+namespace ReSharperPlugin.TestLinker2.Tests
 {
-	protected override string ExtraPath => "Navigation";
-
-	protected override string RelativeTestDataPath => "SingleTestSingleProd";
-
-	[Test]
-	public void SingleTestSingleProd()
+	public class SingleTestSingleProdTests : AllNavigationProvidersTestBase
 	{
-		DoNamedTest("SingleTestSingleProdTests.cs");
+		protected override string ExtraPath => "Navigation";
+
+		protected override string RelativeTestDataPath => "SingleTestSingleProd";
+
+		[Test]
+		public void SingleTestSingleProd()
+		{
+			DoNamedTest("SingleTestSingleProdTests.cs");
+		}
 	}
 }
